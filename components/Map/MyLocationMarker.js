90df97603c680960e13387d096762944b2259850
Shaqeel Less <54390756+MogammadShaqeelless16@users.noreply.github.com>
2024-08-09 17:07:15 +0200

    update

--- components/Map/MyLocationMarker.js
+++ components/Map/MyLocationMarker.js
@@ -1,9 +1,10 @@
 // components/Map/MyLocationMarker.js
 import React from 'react';
-import { Marker, Popup } from 'react-leaflet';
-import L from 'leaflet';
+import { Platform, StyleSheet, View, Text, Image } from 'react-native';
+import { Marker as WebMarker, Popup } from 'react-leaflet';
+import { Marker as MobileMarker } from 'expo-maps';
 
-// Custom icon for the marker
+// Custom icon for the marker (web)
 const myLocationIcon = new L.Icon({
   iconUrl: 'https://cdn.pixabay.com/animation/2023/04/06/16/10/16-10-43-442_512.gif',
   iconSize: [32, 32],
@@ -11,15 +12,42 @@ const myLocationIcon = new L.Icon({
   popupAnchor: [0, -32],
 });
 
-function MyLocationMarker({ location }) {
-  return (
-    <Marker
-      position={[location.latitude, location.longitude]}
-      icon={myLocationIcon}
-    >
-      <Popup>You are here</Popup>
-    </Marker>
-  );
-}
+const MyLocationMarker = ({ location }) => {
+  const lat = parseFloat(location.latitude) || 0;
+  const lon = parseFloat(location.longitude) || 0;
+
+  if (Platform.OS === 'web') {
+    return (
+      <WebMarker
+        position={[lat, lon]}
+        icon={myLocationIcon}
+      >
+        <Popup>You are here</Popup>
+      </WebMarker>
+    );
+  } else {
+    return (
+      <MobileMarker coordinate={{ latitude: lat, longitude: lon }}>
+        <View style={styles.marker}>
+          <Image
+            source={{ uri: 'https://cdn.pixabay.com/animation/2023/04/06/16/10/16-10-43-442_512.gif' }}
+            style={styles.icon}
+          />
+          <Text>You are here</Text>
+        </View>
+      </MobileMarker>
+    );
+  }
+};
+
+const styles = StyleSheet.create({
+  marker: {
+    alignItems: 'center',
+  },
+  icon: {
+    width: 32,
+    height: 32,
+  },
+});
 
 export default MyLocationMarker;
